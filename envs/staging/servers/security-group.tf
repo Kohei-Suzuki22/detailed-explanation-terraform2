d5206f617820fd529ba2d6a7bb201550bd997c9b
kohei <bhkbb0551@gmail.com>
2024-01-02 13:54:02 +0900

    lbを作成

---
+++ envs/staging/servers/security-group.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "web-server" {
+  name = "web-server"
+  vpc_id = data.terraform_remote_state.globals.outputs.terraform_vpc_id
+
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" : "web-server"
+  }
+
+}
+
+
+resource "aws_security_group" "alb" {
+  name = "alb"
+  vpc_id = data.terraform_remote_state.globals.outputs.terraform_vpc_id
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    "Name" : "alb"
+  }
+}
--- envs/staging/servers/web-server.tf
+++ envs/staging/servers/web-server.tf
@@ -32,6 +32,9 @@ resource "aws_autoscaling_group" "web-server" {
   launch_configuration = aws_launch_configuration.web-server.name
   vpc_zone_identifier = data.terraform_remote_state.globals.outputs.subnet_ids
 
+  target_group_arns = [aws_lb_target_group.alb.arn]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -47,28 +50,62 @@ resource "aws_autoscaling_group" "web-server" {
 }
 
 
-resource "aws_security_group" "web-server" {
-  name = "web-server"
-  vpc_id = data.terraform_remote_state.globals.outputs.terraform_vpc_id
-
-
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    "Name" : "web-server"
-  }
-
+resource "aws_lb" "web-server" {
+  name = "web-server-alb"
+  load_balancer_type = "application"
+  subnets = data.terraform_remote_state.globals.outputs.subnet_ids
+  security_groups = [aws_security_group.alb.id]
 }
 
+
+resource "aws_lb_listener" "web-server" {
+  load_balancer_arn = aws_lb.web-server.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+  
+}
+
+resource "aws_lb_listener_rule" "alb" {
+  listener_arn = aws_lb_listener.web-server.arn
+  priority = 100
+  
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+}
+
+resource "aws_lb_target_group" "alb" {
+  name = "web-server-alb-target"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.terraform_remote_state.globals.outputs.terraform_vpc_id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+  
+}
