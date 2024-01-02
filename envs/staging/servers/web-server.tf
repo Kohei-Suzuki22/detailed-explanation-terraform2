29db663a813615bcc8fe0bc7fc4726e34c84fd7c
kohei <bhkbb0551@gmail.com>
2024-01-02 13:18:58 +0900

    ec2 securitygroupをアップデート

--- envs/staging/servers/web-server.tf
+++ envs/staging/servers/web-server.tf
@@ -51,12 +51,12 @@ resource "aws_security_group" "web-server" {
   name = "web-server"
   vpc_id = data.terraform_remote_state.globals.outputs.terraform_vpc_id
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port = 80
+  #   to_port = 80
+  #   protocol = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     from_port = var.server_port
